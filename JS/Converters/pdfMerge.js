7abbca766de4239c4f52316a7ea45f45604cfcd2
Shaial Miah <shaial@hotmail.co.uk>
2026-01-24 08:48:52 +0000

    adding a file to merge will render the file names onto the display as a list in order the files are selected

--- JS/Converters/pdfMerge.js
+++ JS/Converters/pdfMerge.js
@@ -5,6 +5,7 @@ const inputFile = document.getElementById("inputFileUpload");
 const uploadFileTxt = document.getElementById("uploadFileTxt");
 const fileDisplay = document.getElementById("fileDisplay");
 const fileInputSection = document.getElementById("fileInput");
+const fileUnorderedList = document.getElementById("fileUnorderedList");
 
 // clicking the 'select pdf files' button will open file explorer.
 uploadButton.addEventListener("click", () => {
@@ -22,19 +23,38 @@ const hideSelectPdfFilesBtn = () => {
 
 // check if a file has been selected.
 
+/* this array will store the names of the selected
+files to be merged to render them onto the display */
 const fileArray = [];
 
 inputFile.addEventListener("change", () => {
     if (inputFile.files.length > 0) {
-        fileArray.push(inputFile.files[0].name)
 
-        console.log(fileArray);
+        /* loops through the file object array list & finds the 
+        names of selected files.*/
+        for (let i = 0; i < inputFile.files.length; i++) {
+            fileArray.push(inputFile.files[i].name);
+        };
+
         hideSelectPdfFilesBtn();
+        renderFiles();
     }
 });
 
 // render files to browser to see which files have been selected
 
 const renderFiles = () => {
+
+    console.log(fileArray);
+
+    /* loops through fileArray and renders onto page */
+    for (let i = 0; i < fileArray.length; i++) {
+
+        const li = document.createElement("li");
+
+        li.innerHTML = fileArray[i];
+
+        fileUnorderedList.appendChild(li);
+    };
     
-}
+};
