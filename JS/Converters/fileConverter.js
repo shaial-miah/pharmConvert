e5443a5302e9e471ddfc7aed41f7e0de3e6fb809
Shaial Miah <shaial@hotmail.co.uk>
2026-01-26 21:01:02 +0000

    commit all files to repo

---
+++ JS/Converters/fileConverter.js
@@ -0,0 +1,62 @@
+/* variables */
+
+const uploadButton = document.getElementById("uploadFileBtn");
+const inputFile = document.getElementById("inputFileUpload");
+const uploadFileTxt = document.getElementById("uploadFileTxt");
+const fileDisplay = document.getElementById("fileDisplay");
+const fileInputSection = document.getElementById("fileInput");
+const fileUnorderedList = document.getElementById("fileUnorderedList");
+const convertButton = document.getElementById("convertButton");
+
+// clicking the 'select pdf files' button will open file explorer.
+uploadButton.addEventListener("click", () => {
+    inputFile.click();
+});
+
+// hide 'select pdf files' after inputFile is clicked.
+
+const hideSelectPdfFilesBtn = () => {
+    setTimeout(()=> {
+        fileInputSection.remove();
+        fileDisplay.style.display = "block";
+    }, 1000)
+};
+
+// check if a file has been selected.
+
+/* this array will store the names of the selected
+files to be merged to render them onto the display */
+const fileArray = [];
+
+inputFile.addEventListener("change", () => {
+    if (inputFile.files.length > 0) {
+
+        /* loops through the file object array list & finds the 
+        names of selected files.*/
+        for (let i = 0; i < inputFile.files.length; i++) {
+            fileArray.push(inputFile.files[i].name);
+        };
+
+        hideSelectPdfFilesBtn();
+        renderFiles();
+    } 
+});
+
+// render files to browser to see which files have been selected
+
+const renderFiles = () => {
+
+    console.log(fileArray);
+
+    /* loops through fileArray and renders onto page */
+    for (let i = 0; i < fileArray.length; i++) {
+
+            const li = document.createElement("li");
+
+            li.innerHTML = fileArray[i];
+
+            fileUnorderedList.appendChild(li);
+    
+}
+
+};
--- JS/app.js
+++ JS/app.js
@@ -1,9 +1,16 @@
 const mergeTool = document.getElementById("mergeTool");
+const convertTool = document.getElementById("convertTool")
 const pharmConvertLogo = document.getElementById("pharmConvertLogo")
 
 // clicking merge tool selection
 mergeTool.addEventListener("click", () => {
     window.location = "html/merge.html";
 });
+
+// clicking convert tool selection
+convertTool.addEventListener("click", () => {
+    window.location = "html/convert.html";
+});
+
 
 
